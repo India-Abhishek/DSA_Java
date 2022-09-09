e109dd04e18c42f78415002062acd01308665342
Abhishek <abhishekbth99@gmail.com>
2022-09-09 10:46:13 +0530

    compare string and array

---
+++ 08String/String1.java
@@ -0,0 +1,33 @@
+public class String1 {
+   
+    public static void main(String args[]){
+        // we initilize char in single Quote '' but in String is in double Quotes " "
+        //char c='c';
+        String str="hello_world";
+        // Printing character
+        System.out.println(str.charAt(1));
+        // System.out.println(stt[4]); Incorrect
+        //sub String 
+        System.out.println(str.substring(2, 7));
+        // sub string from start to till end
+        // if we want to print the whole string we can start from 0th index
+        System.out.println(str.substring(3)); 
+        // check if character is present in string or not
+        // str.contains return boolen value (true or false )
+        System.out.println(str.contains("hello"));
+        System.out.println(str.contains("hey"));
+        //print whole string
+        System.err.println(str);
+
+        // printing every character one by one from string
+        print(str);
+    }
+
+    public static void print(String s){
+        for(int i=0; i<s.length(); i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+
+    
+}
