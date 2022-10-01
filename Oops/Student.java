6ca3d47d0c99586a1b371dc9b88b4567c189c6ef
Abhishek <abhishekbth99@gmail.com>
2022-10-02 00:48:11 +0530

    commit

---
+++ Oops/Student.java
@@ -0,0 +1,7 @@
+package Oops;
+
+public class Student {
+    String name;
+    int roll;
+    
+}
---
+++ Oops/StudentUse.java
@@ -0,0 +1,12 @@
+package Oops;
+
+public class StudentUse {
+    public static void main(String [] args){
+        // Student s = new Student();
+        // s.name = "Anand";
+        // s.roll = 98;
+        
+        System.out.println("s.name");
+    }
+    
+}
