9cccc233437ee85e3a2fecf4a5056af679e6c45d
Abhishek <abhishekbth99@gmail.com>
2022-10-01 23:40:47 +0530

    Commit

--- 00First Output/First_IO.java
+++ 00First Output/First_IO.java
@@ -1,7 +1,7 @@
 public class First_IO {
     public static void main(String[] args) {
         int x;
-        System.out.println("Enter a Nuber: ");
+        System.out.println("Enter a Number: ");
              
     }
 }
