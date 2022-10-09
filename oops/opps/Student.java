9f0f39f457cead9a7e1a38e9271dcdc85434073b
Abhishek <abhishekbth99@gmail.com>
2022-10-09 09:51:36 +0530

    Commit

--- oops/opps/Student.java
+++ oops/opps/Student.java
@@ -1,7 +1,42 @@
 package opps;
 
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
 public class Student {
     String name;
     int roll;
+
+// Creating Setter function to set object properties
+     public void setName(String n){
+        name = n;
+    }
+
+    public void setamount(int r){
+        roll = r;
+    }
+
+// Creating gettier method to set object properties
+    public int getRoll(){
+        return roll;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+// Creating a constructor
+       public Student(String StudentName, int StudentRoll){
+        name = StudentName;
+        roll = StudentRoll;
+    }
+//>> Since you created ur own constructor The defult constrector will not work any more.
+
+
+// Creating a print function
+    public void print(){
+        System.out.println(name+" : "+roll);
+
+    }
+
     
 }
--- oops/opps/StudentUse.java
+++ oops/opps/StudentUse.java
@@ -1,16 +1,37 @@
 package opps;
 
 public class StudentUse {
-    public static void main(String [] args){
+    public static void main(String [] args){      
+/*
+
         Student s1 = new Student();
         s1.name="Abhishek";
         s1.roll=123;
 
         System.out.println(s1.name+" "+s1.roll);
 
-        // Using constructor creating an object
-        ConstrectorStudent s2 = new ConstrectorStudent("Anand", 8430);
-        System.out.println(s2.name);
+        
+
+        //Creating an object
+        Student s2 = new Student();
+
+        // Calling Setter method to set object properties
+        s2.name ="Aman";
+        s2.roll = 201390;
+
+        // Calling gettier method to print/fetch object properties
+        System.out.println(s2.getName()+" "+s2.getRoll());
+
+*/
+        // Using constructor create an object
+        Student s3 = new Student("Anand", 8430);
+        //System.out.println(s3.name);
+
+        //since you using ur own constructor The defult constrector will not work any more.
+
+        // Print using print function
+        s3.print();
+
     }
     
 }
