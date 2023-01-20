01d3dc2696aa8d4653543a22239175c58b403484
Abhishek <abhishekbth99@gmail.com>
2023-01-20 12:27:58 +0530

    hehe

--- Revision/tranglePattern.java
+++ Revision/tranglePattern.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class tranglePattern {
     public static void main(String[] args) {
-        int n=4;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to print trangle Number patterns: ");
+        int n = sc.nextInt();
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
                 System.out.print(j);
@@ -21,6 +25,18 @@ public class tranglePattern {
             }
             System.out.println();
         }
+
+        System.out.println("Enter a number to print trangle STAR pattern: ");
+        int num = sc.nextInt();
+
+        for(int i=1; i<=num; i++){
+            for( int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+
     }
     
 }
