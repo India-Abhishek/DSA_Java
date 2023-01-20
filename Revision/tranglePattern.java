71774581f3532580746997267170c83b0c3e4251
Abhishek <abhishekbth99@gmail.com>
2023-01-20 12:44:06 +0530

    trangle pattern complited

--- Revision/tranglePattern.java
+++ Revision/tranglePattern.java
@@ -28,13 +28,22 @@ public class tranglePattern {
 
         System.out.println("Enter a number to print trangle STAR pattern: ");
         int num = sc.nextInt();
-
         for(int i=1; i<=num; i++){
             for( int j=1; j<=i; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+
+        System.out.println("Enter a number to print Reverse number pattern: ");
+        int num2 = sc.nextInt();
+        for(int i=1; i<=num2; i++){
+            for( int j=i; j>=1; j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        
         sc.close();
 
     }
