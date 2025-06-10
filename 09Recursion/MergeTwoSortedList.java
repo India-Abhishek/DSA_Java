f4e5256c524fd004bd3fc5a917d9a0dad4f09b3e
Abhishek <abhishekbth99@gmail.com>
2025-06-11 00:52:03 +0530

    pending merge tow sorted list

---
+++ 09Recursion/MergeTwoSortedList.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class MergeTwoSortedList {
+    public static void main(String[] args) {
+
+        private static List<Integer> mergeTwoSortedLists(List<Integer> listA, List<Integer> listB) {
+        
+            if(listA == NULL) return listB;
+            if(listB == NULL) return listA;
+
+            if(listA.)
+        
+    }
+
+
+
+        List<Integer> listA = List.of(1, 3, 5, 7); // Java 9+ for List.of()
+        List<Integer> listB = List.of(2, 4, 6, 8);
+
+        List<Integer> merged = mergeTwoSortedLists(listA, listB);
+    }
+
+    
+    
+}
