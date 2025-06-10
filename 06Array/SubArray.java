0865b72629f02baa80a944dc2eb45376e76acd97
Abhishek <abhishekbth99@gmail.com>
2025-06-10 16:33:44 +0530

    Leetcode 53. Maximum Subarray

--- 06Array/SubArray.java
+++ 06Array/SubArray.java
@@ -15,6 +15,36 @@ public class SubArray {
                 System.out.println();
             }
         }
+
+System.out.println("--maximum subarray sum---Brute force approach = O(n2)--");
+
+        int arr2 [] = {-2,1,-3,4,-1,2,1-5,4};
+        int maxSum = Integer.MIN_VALUE;
+
+        for(int st=0; st<arr2.length; st++){
+            int currSum = 0;
+            for(int end=st; end<arr2.length; end++){
+                currSum += arr2[end];
+                maxSum = Math.max(currSum, maxSum);
+            }
+        }
+
+        System.out.println("maximum sub array sum = "+maxSum);
+
+System.out.println("--maximum subarray sum---Kadane's algorithm = O(n)--");
+
+        int current = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i : arr2){
+            current += i;
+            max = Math.max(current, max);
+            if(current<0){
+                current = 0;
+            }
+        }
+
+        System.out.println(max);
+
     }
     
 }
